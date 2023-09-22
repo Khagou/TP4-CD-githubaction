9f828cd474e33650dbfd48ccf3076d482b4b6824
Maxence <max.verdom@gmail.com>
2023-09-22 16:52:46 +0200

    try deploiement terraform, add token to google auth

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,14 @@
 # main.tf
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
 provider "google" {
+  credentials = file("credential.json")
   project     = var.gcp_project
   region      = var.gcp_region
   zone        = var.gcp_zone
