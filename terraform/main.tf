b1a2dfdc51747de9f09d700fcf718d1fa6e08482
Maxence <max.verdom@gmail.com>
2023-09-25 17:54:36 +0200

    try deploiement terraform, test gcr

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,9 +32,9 @@ module "instances" {
   machine = var.machine
 }
 
-# module "artifact" {
-#   source = "./artifact_registry_repo"
-#   location = var.gcp_region
-#   repository_id = var.docker-repo
-# }
+module "artifact" {
+  source = "./artifact_registry_repo"
+  gcp_region = var.gcp_region
+  docker-repo = var.docker-repo
+}
 
