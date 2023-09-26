e583331994882bba33ee868e9f7ff77ba67427de
Maxence <max.verdom@gmail.com>
2023-09-26 19:18:16 +0200

    try push image to gcr, try comment backend gcs and delete terraform.tfvars and comment api in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,13 +10,13 @@ provider "google" {
 #   project = var.gcp_project
 # }
 module "network" {
-  depends_on = [ module.api ]
+  # depends_on = [ module.api ]
   source       = "./network"
   subnet_cidr = var.subnet_cidr
 }
 
 module "firewall" {
-  depends_on = [ module.api ]
+  # depends_on = [ module.api ]
   source           = "./firewall"
   network_self_link = module.network.network_self_link
   firewall_source = var.firewall_source
