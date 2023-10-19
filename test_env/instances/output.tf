f62e4a2f14414fa5dbc8236cb3432d26a914e183
Maxence <max.verdom@gmail.com>
2023-10-19 11:32:39 +0200

    first try with provisioner

--- test_env/instances/output.tf
+++ test_env/instances/output.tf
@@ -1,3 +1,3 @@
-output "dev_instance_ip" {
-  value = google_compute_instance.dev_instance.network_interface.0.access_config.0.nat_ip
+output "test_instance_ip" {
+  value = google_compute_instance.test_instance.network_interface.0.access_config.0.nat_ip
 }
