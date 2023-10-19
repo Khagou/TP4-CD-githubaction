66fca769dcd28bd165bcf23986f0f7e16436f736
Maxence <max.verdom@gmail.com>
2023-10-19 11:18:49 +0200

    check env ls pwd for terraform provisioner

--- test_env/instances/instances.tf
+++ test_env/instances/instances.tf
@@ -3,17 +3,6 @@
 ############# DEV INSTANCE CONFIG ###############
 #################################################
 
-module "gce-container" {
-  source = "terraform-google-modules/container-vm/google"
-  version = "~> 2.0"  # Upgrade the version if necessary.
-
-  container = {
-    image = "europe-west1-docker.pkg.dev/tp4-cd-400812/docker-repo/tp4-cd:latest"
-  }
-
-  cos_image_name = "cos-109-17800-0-45"
-}
-
 resource "google_compute_instance" "test_instance" {
   name         = var.test
   machine_type = var.machine
@@ -31,17 +20,18 @@ resource "google_compute_instance" "test_instance" {
       # Autoriser l'accès par une adresse IP externe
     }
   }
-    metadata = {
-    # Required metadata key.
-    gce-container-declaration = module.gce-container.metadata_value
-    google-logging-enabled    = "true"
-    google-monitoring-enabled = "true"
+    provisioner "file" {
+    source      = "compose-app/"
+    destination = "/app"
   }
 
-  labels = {
-    # Required label key.
-    container-vm = module.gce-container.vm_container_label
+  provisioner "remote-exec" {
+    inline = [
+      "cd /app",
+      "docker-compose up",
+    ]
   }
+
   service_account {
     email = var.sa_email
     scopes = [
