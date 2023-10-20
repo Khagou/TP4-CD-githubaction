8124ab60acdab4cc1e5f3cb483a9c3cfa8538c2c
Maxence <max.verdom@gmail.com>
2023-10-20 17:08:48 +0200

    first try with ubuntu and manual install for docker and compose

--- test_env/instances/instances.tf
+++ test_env/instances/instances.tf
@@ -31,17 +31,13 @@ resource "google_compute_instance" "test_instance" {
   metadata = {
     ssh-keys = "${file(var.pub_key)}"
   }
-  provisioner "file" {
-    connection {
-      host        = google_compute_instance.test_instance.network_interface.0.access_config.0.nat_ip
-      type        = "ssh"
-      user        = var.sa_email
-      private_key = var.private_key
-    }
-    source = "/home/runner/work/TP4-CD-githubaction/TP4-CD-githubaction"
-    destination = "~/TP4-CD-githubaction"
-  }
 
+}
+
+resource "null_resource" "upload_files" {
+  triggers = {
+    instance_id = google_compute_instance.test_instance.id
+  }
   provisioner "remote-exec" {
     connection {
       host        = google_compute_instance.test_instance.network_interface.0.access_config.0.nat_ip
@@ -66,16 +62,30 @@ resource "google_compute_instance" "test_instance" {
   }
 }
 
-# resource "null_resource" "upload_files" {
-#   triggers = {
-#     instance_id = google_compute_instance.test_instance.id
-#   }
+resource "null_resource" "remote_exec" {
+  triggers = {
+    instance_id = google_compute_instance.test_instance.id
+  }
+  provisioner "remote-exec" {
+    connection {
+      host        = google_compute_instance.test_instance.network_interface.0.access_config.0.nat_ip
+      type        = "ssh"
+      user        = var.sa_email
+      private_key = var.private_key
+    }
 
-# }
-
-# resource "null_resource" "remote_exec" {
-#   triggers = {
-#     instance_id = google_compute_instance.test_instance.id
-#   }
-
-# }
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install curl",
+      "sudo apt-get install gnupg",
+      "sudo apt-get install ca-certificates",
+      "sudo apt-get install lsb-release",
+      "sudo mkdir -p /etc/apt/keyrings",
+      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg",
+      "echo 'deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-pluginsudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-pluginlinux/ubuntu   $(lsb_release -cs) stable' | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
+      "sudo apt-get update",
+      "sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin",
+      "cd /TP4-CD-githubaction/docker-test && docker-compose up",
+    ]
+  }
+}
