a9b01feca64fc3527c778f3c6b248fb4cad50695
Maxence <max.verdom@gmail.com>
2023-10-26 12:31:18 +0200

    first try build gke env, try use gcp_zone to quotas SSD

--- prod_env/gke_cluster/variables.tf
+++ prod_env/gke_cluster/variables.tf
@@ -6,6 +6,10 @@ variable "gcp_region" {
   description = "Region name"
 }
 
+variable "gcp_zone" {
+  description = "Zone name"
+}
+
 variable "node_pool_name" {
   description = "node pool name"
 }
