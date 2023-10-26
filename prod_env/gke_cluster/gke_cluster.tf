b80cfaba7e15f7cdfae0f5be2be5491f1231a725
Maxence <max.verdom@gmail.com>
2023-10-26 14:14:14 +0200

    first try build gke env, try use gcp_zone to quotas SSD

--- prod_env/gke_cluster/gke_cluster.tf
+++ prod_env/gke_cluster/gke_cluster.tf
@@ -2,21 +2,21 @@ resource "google_service_account" "gke" {
   account_id   = "gke-service-account"
   display_name = "Service Account for GKE"
 }
-resource "google_project_iam_binding" "gke_container_admin" {
+resource "google_project_iam_binding" "gke" {
   project  = var.gcp_project
   role               = "roles/container.admin"
    members = [
     "serviceAccount:${google_service_account.gke.email}",
   ]
 }
-resource "google_project_iam_binding" "gke_storage_admin" {
+resource "google_project_iam_binding" "gke" {
   project  = var.gcp_project
   role               = "roles/storage.admin"
    members = [
     "serviceAccount:${google_service_account.gke.email}",
   ]
 }
-resource "google_project_iam_binding" "gke_cluster_viewer" {
+resource "google_project_iam_binding" "gke" {
   project  = var.gcp_project
   role               = "roles/container.clusterViewer"
    members = [
