dcab6d8e2a9f3d824af93f336673d760148359c0
Maxence <max.verdom@gmail.com>
2023-11-13 16:18:09 +0100

    last test dev

--- env_base/versions.tf
+++ env_base/versions.tf
@@ -3,7 +3,7 @@
 terraform {
   required_version = ">= 0.13"
   backend "gcs" {
-    bucket = "tp4tfstate" # Change me
+    bucket = "tp4testbucket" # Change me
     prefix = "terraform-env-base/tfstate"
   }
   
